5e61a4f760790d52145e7bb2abe18225fc9b18e3
pierco <pier.cornier@gmail.com>
2019-05-14 01:08:18 -0400

    fixed rx

--- uart_rx.v
+++ uart_rx.v
@@ -5,74 +5,70 @@ module uart_rx (
   input clk,
   input rx,
   output reg [7:0] dout,
-  output done
+  output reg done
 );
 
 reg [2:0] state;
 reg [2:0] new_state;
 reg [7:0] SR;
 reg [2:0] SC;
-reg [4:0] cycles;
+reg [9:0] cycles;
+
+parameter BCLK = 10'd434 - 1;
+parameter HBCLK = 10'd217 - 1;
 
 parameter
   IDLE   = 3'd0,
-  READ   = 3'd1,
-  WAIT1  = 3'd2,
-  WAIT2  = 3'd3,
-  STOP   = 3'd4;
-
-
-reg [4:0] clk_cnt;
-wire clk_en;
+  START  = 3'd1,
+  READ   = 3'd2,
+  STOP   = 3'd3,
+  DONE   = 3'd4;
 
 always @(posedge clk)
-  if (clk_cnt == 5'd27) // 50M / 115200 / 16 = 27
-    clk_cnt <= 5'd0;
-  else
-    clk_cnt <= clk_cnt + 5'd1;
-
-assign clk_en = clk_cnt == 5'd0;
-
-always @(posedge clk_en)
   state <= new_state;
 
-assign done = state == STOP;
-
-always @*
+always @(posedge clk)
   case (state)
-    IDLE: if (rx == 0) new_state = WAIT1;
-    READ:
-      if (SC == 3'd7)
-        new_state = STOP;
+    IDLE:
+      if (rx == 0) begin
+        new_state <= START;
+        cycles <= 0;
+        SR <= 0;
+        SC <= 0;
+      end
+    START:
+      if (cycles == HBCLK) begin
+        cycles <= 0;
+        if (rx == 0)
+          new_state <= READ;
+        else
+          new_state <= IDLE;
+      end
       else
-        new_state = WAIT2;
-    WAIT1: if (cycles == 5'd21) new_state = READ;
-    WAIT2: if (cycles == 5'd14) new_state = READ;
-    STOP: if (cycles == 5'd12) new_state = IDLE;
+        cycles <= cycles + 1;
+    READ:
+      if (cycles == BCLK) begin
+        cycles <= 0;
+        SR <= { rx , SR[7:1] };
+        SC <= SC + 1;
+        if (SC == 3'd7)
+          new_state <= STOP;
+      end
+      else
+        cycles <= cycles + 1;
+    STOP:
+      if (cycles == BCLK) begin
+        cycles <= 0;
+        new_state <= DONE;
+        done <= 1;
+        dout <= SR;
+      end
+      else
+        cycles <= cycles + 1;
+    DONE: begin
+      new_state <= IDLE;
+      done <= 0;
+    end
   endcase
 
-always @(posedge clk_en)
-  case (state)
-    IDLE,
-    READ: cycles <= 0;
-    WAIT1,
-    WAIT2,
-    STOP: cycles <= cycles + 5'd1;
-  endcase
-
-always @(posedge clk_en)
-  case (state)
-    IDLE: SR <= 8'b0;
-    READ: SR <= { rx , SR[7:1] };
-  endcase
-
-always @(posedge clk_en)
-  case (state)
-    IDLE: SC <= 3'd0;
-    READ: SC <= SC + 3'd1;
-  endcase
- 
-always @(posedge clk_en)
-  if (state == STOP && cycles == 5'd12) dout <= SR;
-  
 endmodule
